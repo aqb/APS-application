0f8d6324c006d8757d1638081c97215569e6c1db
Matheus Vinícius <matheusvncs160@gmail.com>
2022-08-09 19:58:59 -0300

    FabricaRepositorios implementada + Setup com FabricaRepositoriosInMemory

--- ProjetoRUP/musicshop-backend/src/setup.ts
+++ ProjetoRUP/musicshop-backend/src/setup.ts
@@ -1,26 +1,8 @@
 import { container } from "tsyringe";
 
-import IRepositorioCarrinhos from "./dados/Carrinho/IRepositorioCarrinhos";
-import RepositorioCarrinhosInMemory from "./dados/Carrinho/RepositorioCarrinhosInMemory/RepositorioCarrinhosInMemory";
-import IRepositorioClientes from "./dados/Cliente/IRepositorioClientes";
-import RepositorioClientesInMemory from "./dados/Cliente/RepositorioClientesInMemory/RepositorioClientesInMemory";
-import IRepositorioEstoque from "./dados/Estoque/IRepositorioEstoque";
-import RepositorioEstoqueInMemory from "./dados/Estoque/RepositorioEstoqueInMemory/RepositorioEstoqueInMemory";
-import IRepositorioPedidos from "./dados/Pedido/IRepositorioPedidos";
-import RepositorioPedidosInMemory from "./dados/Pedido/RepositorioPedidosInMemory/RepositorioPedidosInMemory";
+import FabricaRepositoriosInMemory from "./negocio/Fabricas/FabricaRepositoriosInMemory";
+import IFabricaRepositorios from "./negocio/Fabricas/IFabricaRepositorios";
 
-container.register<IRepositorioClientes>("RepositorioClientes", {
-  useClass: RepositorioClientesInMemory
-});
-
-container.register<IRepositorioPedidos>("RepositorioPedidos", {
-  useClass: RepositorioPedidosInMemory
-});
-
-container.register<IRepositorioEstoque>("RepositorioEstoque", {
-  useClass: RepositorioEstoqueInMemory
-});
-
-container.register<IRepositorioCarrinhos>("RepositorioCarrinhos", {
-  useClass: RepositorioCarrinhosInMemory
+container.register<IFabricaRepositorios>("FabricaRepositorios", {
+  useClass: FabricaRepositoriosInMemory
 });
